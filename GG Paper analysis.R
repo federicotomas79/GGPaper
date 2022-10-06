4d153bd781b6215fabf646316b7b9670621eae35
federicotomas79 <federicotomas79@users.noreply.github.com>
2022-10-06 16:24:29 +1300

    Update GG Paper analysis.R
    New changes

--- GG Paper analysis.R
+++ GG Paper analysis.R
@@ -13,9 +13,12 @@ glimpse(gg1)
 
 #Calculate and add difference in sampling days
 gg.sample.days <- as.Date(as.character(gg1$gg_sample_Date), format="%d/%m/%Y")
+unique(gg.sample.days)
 rs.sample.days <- as.Date(as.character(gg1$rs_sample_Date), format="%d/%m/%Y")
+unique(rs.sample.days)
 
-diff.days.sample <- difftime(gg.sample.days, rs.sample.days, units = "days")
+diff.days.sample <- difftime(rs.sample.days, gg.sample.days, units = "days")
+unique(diff.days.sample)
 gg1$diff.days.sample <- as.numeric(diff.days.sample)
 glimpse(gg1)
 
@@ -23,10 +26,13 @@ glimpse(gg1)
 library(reshape2)
 
 melt.gg1 <- melt(gg1[, c(9,16:25,29)])
+melt.gg1$year <- gg1$year
 
-ggplot(data = melt.gg1, aes(x = value)) + 
-       stat_density() + 
-       facet_wrap(~variable, scales = "free")
+ggplot(data = melt.gg1, aes(x = value, group=year, colour=year)) +
+       stat_density(geom = "path", position = "identity") + 
+       theme_bw() +
+       scale_color_gradientn(colours = rainbow(5))+
+       facet_wrap(~variable, scales = "free") 
 
 #Create correlations of all of the numerical variables
 library(GGally)
